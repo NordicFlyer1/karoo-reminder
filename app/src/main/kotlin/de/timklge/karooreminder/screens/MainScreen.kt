f8f93f7ecea3749796fec50a6efc1954c000b2e7
timklge <2026103+timklge@users.noreply.github.com>
2025-01-17 16:48:16 +0100

    ref #11, ref #12: Make bright colors brighter (#18)
    * ref #11, ref #12: Make bright colors brighter
    
    * Fix color mapping, update version

--- app/src/main/kotlin/de/timklge/karooreminder/screens/MainScreen.kt
+++ app/src/main/kotlin/de/timklge/karooreminder/screens/MainScreen.kt
@@ -44,7 +44,7 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.core.graphics.toColor
+import androidx.core.content.ContextCompat
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -55,6 +55,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import de.timklge.karooreminder.KarooReminderExtension
+import de.timklge.karooreminder.R
 import de.timklge.karooreminder.dataStore
 import de.timklge.karooreminder.streamUserProfile
 import io.hammerhead.karooext.KarooSystemService
@@ -89,6 +90,11 @@ fun ReminderAppNavHost(modifier: Modifier = Modifier, navController: NavHostCont
                 val entries = Json.decodeFromString<MutableList<Reminder>>(
                     t[preferencesKey] ?: defaultReminders
                 )
+                entries.forEach {
+                    if (it.displayForegroundColor == null){
+                        it.displayForegroundColor = ReminderColor.getColor(ctx, it.foregroundColor)
+                    }
+                }
                 reminders.addAll(entries)
             } catch(e: Throwable){
                 Log.e(KarooReminderExtension.TAG,"Failed to read preferences", e)
@@ -106,8 +112,6 @@ fun ReminderAppNavHost(modifier: Modifier = Modifier, navController: NavHostCont
             val reminderId = stack.arguments?.getInt("id")
             val reminder = reminders.find { it.id  == reminderId}
             val reminderIndex = reminders.indexOf(reminder)
-
-            val ctx = LocalContext.current
 
             reminder?.let { r ->
                 DetailScreen(false, r, { updatedReminder ->
@@ -127,8 +131,6 @@ fun ReminderAppNavHost(modifier: Modifier = Modifier, navController: NavHostCont
         composable(route = "create") {
             val nextReminderId = reminders.maxOfOrNull { it.id + 1 } ?: 0
             val newReminder = Reminder(nextReminderId, "", 30, "")
-
-            val ctx = LocalContext.current
 
             DetailScreen(true, newReminder, { updatedReminder ->
                 updatedReminder?.let { r ->
@@ -192,7 +194,7 @@ fun MainScreen(reminders: MutableList<Reminder>, onNavigateToReminder: (r: Remin
                             Modifier
                                 .height(60.dp)
                                 .padding(5.dp), verticalAlignment = Alignment.CenterVertically) {
-                            Surface(shape = CircleShape, color = Color(reminder.foregroundColor.toColor().toArgb()),
+                            Surface(shape = CircleShape, color = Color(ContextCompat.getColor(ctx, reminder.displayForegroundColor?.colorRes ?: R.color.hRed)),
                                 modifier = Modifier
                                     .height(60.dp)
                                     .shadow(5.dp, CircleShape)
